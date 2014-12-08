54e43457950eef7edb66b60f7ec7ca030420e886
Stowasser Jonas <nfss@gmx.de>
2014-12-08 20:40:51 +0100

    Added "Variables" in Apples.java

---
+++ Boston/src/net/noxumbrarum/vid/vid5/Apples.java
@@ -0,0 +1,16 @@
+package net.noxumbrarum.vid.vid5;
+
+public class Apples {
+	public static void main(String args[]){
+		
+		double tuna;
+		
+		tuna = 5.28;
+		
+		System.out.print("I want ");
+		System.out.print(tuna);
+		System.out.println(" movies.");
+		System.out.println("apples");
+		
+	}
+}
