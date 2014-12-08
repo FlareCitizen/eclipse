165ce7b4f9369b6a99548d01f983daf0b63cee86
Stowasser Jonas <nfss@gmx.de>
2014-12-08 21:49:59 +0100

    Added "Increment operators"

---
+++ Boston/src/net/noxumbrarum/vid/vid9/Apples.java
@@ -0,0 +1,18 @@
+package net.noxumbrarum.vid.vid9;
+
+import java.util.Scanner;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+		int tuna = 5;
+		int bass = 18;
+		
+		++tuna;
+		
+		System.out.println(tuna);
+
+	}
+
+}
