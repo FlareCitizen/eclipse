0cc2fc7bb68f453c73191611795ab6d99df7c99f
Stowasser Jonas <nfss@gmx.de>
2014-12-08 21:26:10 +0100

    added user input example

---
+++ Boston/src/net/noxumbrarum/vid/vid6/Apples.java
@@ -0,0 +1,11 @@
+package net.noxumbrarum.vid.vid6;
+
+import java.util.Scanner;
+
+public class Apples {
+	public static void main(String args[]){
+		Scanner scanner1 = new Scanner(System.in);
+		
+		System.out.println(scanner1.nextLine());
+	}
+}
