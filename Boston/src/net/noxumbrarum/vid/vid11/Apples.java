83be0a6ee3623ff4797acc817275218542bb787e
Stowasser Jonas <nfss@gmx.de>
2014-12-08 22:07:07 +0100

    Added advanced operators
    && and || (alt + 7)

---
+++ Boston/src/net/noxumbrarum/vid/vid11/Apples.java
@@ -0,0 +1,20 @@
+package net.noxumbrarum.vid.vid11;
+
+public class Apples {
+
+	public static void main(String[] args) {
+	
+		int boy, girl;
+		
+		boy = 18;
+		girl = 20;
+		
+		if(boy >= 16 || girl <= 20){
+			System.out.println("You can enter!");
+		}else{
+			System.out.println("You cannot enter!");
+		}
+
+	}
+
+}
