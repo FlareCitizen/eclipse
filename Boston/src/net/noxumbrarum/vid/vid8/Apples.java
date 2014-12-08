d23d076f170e9b17d5558578f0af4086168118d5
Stowasser Jonas <nfss@gmx.de>
2014-12-08 21:47:44 +0100

    Added "Math operators"

---
+++ Boston/src/net/noxumbrarum/vid/vid8/Apples.java
@@ -0,0 +1,19 @@
+package net.noxumbrarum.vid.vid8;
+
+import java.util.Scanner;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+		double girls, boys, people;
+		
+		girls = 11;
+		boys = 3;
+		people = girls % boys;
+		
+		System.out.println(people);
+
+	}
+
+}
