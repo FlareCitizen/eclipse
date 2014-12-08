a75d44704b1a9b5efb3bf4624f823c5d8ca85c4e
Stowasser Jonas <nfss@gmx.de>
2014-12-08 21:59:57 +0100

    added "if statement"

---
+++ Boston/src/net/noxumbrarum/vid/vid10/Apples.java
@@ -0,0 +1,17 @@
+package net.noxumbrarum.vid.vid10;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		
+		int test = 6;
+		
+		if(test < 9) {
+			System.out.println("yes");
+		}else{
+			System.out.println("This is else");
+		}
+
+	}
+
+}
