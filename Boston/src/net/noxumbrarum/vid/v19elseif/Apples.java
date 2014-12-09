e4674f0167446c1ad915ac8c6f03cc868a139e4e
Stowasser Jonas <nfss@gmx.de>
2014-12-09 17:45:28 +0100

    Added "else-if"

---
+++ Boston/src/net/noxumbrarum/vid/v19elseif/Apples.java
@@ -0,0 +1,22 @@
+package net.noxumbrarum.vid.v19elseif;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		int age = 55;
+		if(age >= 60){
+			System.out.println("You are senior citizen");
+		}
+		else if(age >= 50){
+			System.out.println("You are in your 50s");
+		}
+		else if(age >= 40){
+			System.out.println("You are in your 40s");
+		}
+		else{
+			System.out.println("You are a young buck");
+		}
+
+	}
+
+}
