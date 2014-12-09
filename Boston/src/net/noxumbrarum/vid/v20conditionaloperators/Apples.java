7d1ffa4d4b881ee4649f52cb92bce7866145e503
Stowasser Jonas <nfss@gmx.de>
2014-12-09 17:50:56 +0100

    Added "Conditional Operator"

---
+++ Boston/src/net/noxumbrarum/vid/v20conditionaloperators/Apples.java
@@ -0,0 +1,13 @@
+package net.noxumbrarum.vid.v20conditionaloperators;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		int age = 21;
+		
+		System.out.println(age > 50 ? "You are old" : "You are young");
+		
+
+	}
+
+}
