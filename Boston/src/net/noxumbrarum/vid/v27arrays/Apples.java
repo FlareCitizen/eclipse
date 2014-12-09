2bd7187ebf492b58589b9885151f69d6771b7f06
Stowasser Jonas <nfss@gmx.de>
2014-12-09 23:33:19 +0100

    Added "Array"

---
+++ Boston/src/net/noxumbrarum/vid/v27arrays/Apples.java
@@ -0,0 +1,18 @@
+package net.noxumbrarum.vid.v27arrays;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		int[] array = new int[10];
+		int[] brray = {2, 4, 5, 6, 7, 8, 9};
+		
+		array[0] = 78;
+		array[1] = 543;
+		
+		
+		System.out.println(array[1]);
+		System.out.println(brray[1]);
+
+	}
+
+}
