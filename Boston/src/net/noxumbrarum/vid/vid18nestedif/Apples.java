2b51f325e5f27ef05ba00042088284ac032d3c54
Stowasser Jonas <nfss@gmx.de>
2014-12-09 03:05:33 +0100

    Added "Nested if"

---
+++ Boston/src/net/noxumbrarum/vid/vid18nestedif/Apples.java
@@ -0,0 +1,21 @@
+package net.noxumbrarum.vid.vid18nestedif;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		int age = 60;
+		
+		if(age < 50){
+			System.out.println("You are young");
+		}else{
+			System.out.println("You are old");
+			if(age > 75){
+				System.out.println("You are really old");
+			}else{
+				System.out.println("Don't worry you aren't really that old");
+			}
+		}
+
+	}
+
+}
