cbccccda4b687153651785d9b63b1b1c3718b176
Stowasser Jonas <nfss@gmx.de>
2014-12-09 02:09:18 +0100

    Added parameters to methods

---
+++ Boston/src/net/noxumbrarum/vid/vid15parameterclasses/Apples.java
@@ -0,0 +1,19 @@
+package net.noxumbrarum.vid.vid15parameterclasses;
+
+import java.util.Scanner;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		tuna tunaObj = new tuna();
+		String name;
+		
+		System.out.print("Enter your name here: ");
+		name = input.nextLine();
+		
+		tunaObj.simpleMsg(name);
+		System.out.println("!");
+	}
+
+}
---
+++ Boston/src/net/noxumbrarum/vid/vid15parameterclasses/tuna.java
@@ -0,0 +1,7 @@
+package net.noxumbrarum.vid.vid15parameterclasses;
+
+public class tuna {
+	public void simpleMsg(String name){
+		System.out.println("Hello " + name);
+	}
+}
