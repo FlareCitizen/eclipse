dedbd655d47a5a30ef01f6011a6b77346e026ddc
Stowasser Jonas <nfss@gmx.de>
2014-12-09 23:18:53 +0100

    Added "mvp math methods"

---
+++ Boston/src/net/noxumbrarum/vid/v25mathclass/Apples.java
@@ -0,0 +1,15 @@
+package net.noxumbrarum.vid.v25mathclass;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-26.7));
+		System.out.println(Math.ceil(26.7)); //round up
+		System.out.println(Math.floor(26.7)); // round down
+		System.out.println(Math.max(26, 5.2)); //returns maximum
+		System.out.println(Math.min(26.7, 48)); // returns minimum
+		System.out.println(Math.pow(2, 2)); // ^
+		System.out.println(Math.sqrt(81)); // wurzel
+	}
+
+}
