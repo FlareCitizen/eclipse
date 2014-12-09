96c3e1e42a1f17c4377e7823c6d646ef04a166d4
Stowasser Jonas <nfss@gmx.de>
2014-12-09 23:13:18 +0100

    Added "do-while"

---
+++ Boston/src/net/noxumbrarum/vid/v24dowhile/Apples.java
@@ -0,0 +1,15 @@
+package net.noxumbrarum.vid.v24dowhile;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		int c = 0;
+		
+		do{
+			System.out.println(c);
+			c++;
+		}while(c <= 10);
+
+	}
+
+}
