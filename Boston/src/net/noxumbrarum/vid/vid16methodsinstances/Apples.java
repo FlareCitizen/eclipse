e081e654afe674124e385c0d5e7e50ee1dcee8c7
Stowasser Jonas <nfss@gmx.de>
2014-12-09 02:43:59 +0100

    Split apart for more visibility

--- Boston/src/net/noxumbrarum/vid/vid16methodsinstances/Apples.java
+++ Boston/src/net/noxumbrarum/vid/vid16methodsinstances/Apples.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Apples {
 
 	public static void main(String[] args) {
+		String temp;
+		
 		Scanner input = new Scanner(System.in);
 		tuna tunaObj = new tuna();
 		
 		System.out.println("Enter name of your first girlfriend: ");
-		String temp = input.nextLine();
+		temp = input.nextLine();
 		
 		tunaObj.setGirlName(temp);
 		tunaObj.saying();
