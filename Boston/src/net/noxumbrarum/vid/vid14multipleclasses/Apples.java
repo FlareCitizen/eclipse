57f778039c644ecf2d2e59e92b20aeec9cbb8ef6
Stowasser Jonas <nfss@gmx.de>
2014-12-09 02:00:33 +0100

    Added main class

---
+++ Boston/src/net/noxumbrarum/vid/vid14multipleclasses/Apples.java
@@ -0,0 +1,12 @@
+package net.noxumbrarum.vid.vid14multipleclasses;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		tuna tunaObj = new tuna();
+		
+		tunaObj.simpleMsg();
+
+	}
+
+}
